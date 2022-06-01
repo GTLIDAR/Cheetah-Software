029942122b0b6f2ba157ad999153e3022c56e40f
Ziyi Zhou <zhouziyi@gatech.edu>
2022-06-01 17:09:39 -0400

    tested hardware through 20.04; state estimation offset still exists

--- scripts/ipconfig.sh
+++ scripts/ipconfig.sh
@@ -6,6 +6,6 @@ sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
 sudo ifconfig enx00e04c82a7f7 down
 sudo ifconfig enx00e04c82a7f7 up 192.168.123.162 netmask 255.255.255.0
 
-sudo ifconfig enx00e04c82a7f7 multicast
-sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enx00e04c82a7f7
+# sudo ifconfig enx00e04c82a7f7 multicast
+# sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enx00e04c82a7f7
 
